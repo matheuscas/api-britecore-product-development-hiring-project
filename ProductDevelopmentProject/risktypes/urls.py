e8f3da11bf28b54b62f707bbd3169347f32ad1c5
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-08-29 17:59:14 -0300

    changed views in risktypes to use drf view

--- ProductDevelopmentProject/risktypes/urls.py
+++ ProductDevelopmentProject/risktypes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
+from .views import RiskTypeView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('risktypes', RiskTypeView.as_view(), name='get-riskTypes')
 ]
