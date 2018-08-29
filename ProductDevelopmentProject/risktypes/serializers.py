bb24f29a939c82b0f37a3e0160b554ca90b6c7c3
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-08-29 18:17:22 -0300

    bring all related data to risktypes

--- ProductDevelopmentProject/risktypes/serializers.py
+++ ProductDevelopmentProject/risktypes/serializers.py
@@ -2,6 +2,30 @@ from rest_framework import serializers
 from .models import RiskType
 
 class RiskTypeSerializer(serializers.ModelSerializer):
+    text_fields = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+
+    number_fields = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+
+    date_fields = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+
+    enum_fields = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+
     class Meta:
         model = RiskType
         fields = '__all__'
