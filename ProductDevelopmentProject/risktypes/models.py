f74528c6838f830f2e252d46eaffea283ea55555
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-08-29 18:17:08 -0300

    fname foreing keys for each field type

--- ProductDevelopmentProject/risktypes/models.py
+++ ProductDevelopmentProject/risktypes/models.py
@@ -6,24 +6,26 @@ class RiskType(models.Model):
     description = models.CharField(max_length=200)
 
 class Field(models.Model):
-    risk_type = models.ForeignKey(RiskType, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     class Meta:
         abstract = True
 
 class TextField(Field):
+    risk_type = models.ForeignKey(RiskType, related_name="text_fields", on_delete=models.CASCADE)
     value = models.CharField(max_length=200, blank=True)
 
 class NumberField(Field):
+    risk_type = models.ForeignKey(RiskType, related_name="number_fields", on_delete=models.CASCADE)
     value = models.FloatField(blank=True, null=True)
 
 class DateField(Field):
+    risk_type = models.ForeignKey(RiskType, related_name="date_fields", on_delete=models.CASCADE)
     value = models.DateField(blank=True, null=True)
 
 class EnumField(Field):
-    pass
+    risk_type = models.ForeignKey(RiskType, related_name="enum_fields", on_delete=models.CASCADE)
 
 class EnumFieldValue(models.Model):
-    enum_field = models.ForeignKey(EnumField, on_delete=models.CASCADE)
+    enum_field = models.ForeignKey(EnumField, related_name="enum_field_values", on_delete=models.CASCADE)
     key = models.IntegerField()
     value = models.CharField(max_length=100)
