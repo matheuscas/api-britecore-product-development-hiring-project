dffd30891f324f8f797133577956841813e4fa18
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-08-29 22:22:09 -0300

    added EnumField and EnumFieldValue to admin

--- ProductDevelopmentProject/risktypes/admin.py
+++ ProductDevelopmentProject/risktypes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RiskType, TextField, NumberField, DateField
+from .models import RiskType, TextField, NumberField, DateField, EnumField, EnumFieldValue
 
 # Register your models here.
 
@@ -7,3 +7,5 @@ admin.site.register(RiskType)
 admin.site.register(TextField)
 admin.site.register(NumberField)
 admin.site.register(DateField)
+admin.site.register(EnumField)
+admin.site.register(EnumFieldValue)
