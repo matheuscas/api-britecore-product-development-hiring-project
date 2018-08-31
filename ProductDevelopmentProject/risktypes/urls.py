2645e3237e68e2edff7ba7326f3503f8369bd24a
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-08-30 21:39:32 -0300

    slash at the end

--- ProductDevelopmentProject/risktypes/urls.py
+++ ProductDevelopmentProject/risktypes/urls.py
@@ -4,6 +4,6 @@ from . import views
 from .views import RiskTypeView, RiskTypeDetailView
 
 urlpatterns = [
-    path('risktypes', RiskTypeView.as_view(), name='get-all-riskTypes'),
+    path('risktypes/', RiskTypeView.as_view(), name='get-all-riskTypes'),
     path('risktypes/<int:pk>', RiskTypeDetailView.as_view(), name='get-riskType')
 ]
