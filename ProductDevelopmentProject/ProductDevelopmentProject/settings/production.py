867f2a4229a83e6d2c7fea14f55a8b3d481d13f9
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-09-04 17:32:17 -0300

    added some django deploy checklist recommendations

--- ProductDevelopmentProject/ProductDevelopmentProject/settings/production.py
+++ ProductDevelopmentProject/ProductDevelopmentProject/settings/production.py
@@ -13,3 +13,13 @@ ALLOWED_HOSTS = []
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+X_FRAME_OPTIONS = 'DENY'
