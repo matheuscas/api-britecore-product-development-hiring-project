cf2f7526f85ee41efb8769d2dd88fece50148a29
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-09-04 10:59:48 -0300

    removed value from EnumFieldvalue by mistake

--- ProductDevelopmentProject/risktypes/models.py
+++ ProductDevelopmentProject/risktypes/models.py
@@ -30,6 +30,7 @@ class EnumField(Field):
 
 class EnumFieldValue(models.Model):
     enum_field = models.ForeignKey(EnumField, related_name="enum_field_values", on_delete=models.CASCADE)
+    value = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f'{self.value}'
