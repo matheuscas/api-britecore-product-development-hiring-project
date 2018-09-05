d3d3b5e05a046d9cd0b109bc0270d37934fe7f41
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-09-04 23:50:17 -0300

    propoer prod db definition in settings

--- ProductDevelopmentProject/ProductDevelopmentProject/settings/production.py
+++ ProductDevelopmentProject/ProductDevelopmentProject/settings/production.py
@@ -7,12 +7,16 @@ DEBUG = False
 # It ust be defined after deploy is done
 ALLOWED_HOSTS = []
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("POSTGRES_DB", ''),
+        'USER': os.environ.get("POSTGRES_USER", ''),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", ''),
+        'HOST': os.environ.get("POSTGRES_HOST", 'db'),
+        'PORT': os.environ.get("POSTGRES_PORT", '5432'),
+    }
+}
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
