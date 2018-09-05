5a737e271ee079cb8c99bb5378974b59ffb1e46a
Matheus Cardoso <matheuscas@Matheuss-MacBook-Pro.local>
2018-09-05 17:57:44 -0300

    s3 config to host static

--- ProductDevelopmentProject/ProductDevelopmentProject/settings/production.py
+++ ProductDevelopmentProject/ProductDevelopmentProject/settings/production.py
@@ -18,12 +18,24 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS += [
+    "storages"
+]
+
+AWS_STORAGE_BUCKET_NAME = 'zappa-britecore-s3-static'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
 SECURE_BROWSER_XSS_FILTER = True
 
-SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 
-CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 X_FRAME_OPTIONS = 'DENY'
